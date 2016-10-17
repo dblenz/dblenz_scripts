7e5b78e6722691097b8fe11825c3fa8439733d34
Dustin Lenz <dule@cypress.com>
2016-10-17 11:52:26 -0500

    Commands to add device variables to computer devices in SCCM

---
+++ powershell/work/SetDeviceVariables.ps1
@@ -0,0 +1,6 @@
+New-CMDeviceVariable -DeviceName BIZ-APDF103 -VariableName OSDAdapter0DNSServerList -VariableValue "10.247.26.248,10.248.28.243"
+New-CMDeviceVariable -DeviceName BIZ-APDF103 -VariableName OSDAdapter0EnableDHCP -VariableValue "False"
+New-CMDeviceVariable -DeviceName BIZ-APDF103 -VariableName OSDAdapter0Gateways -VariableValue "10.247.25.254"
+New-CMDeviceVariable -DeviceName BIZ-APDF103 -VariableName OSDAdapter0IPAddressList -VariableValue "10.247.25.85"
+New-CMDeviceVariable -DeviceName BIZ-APDF103 -VariableName OSDAdapter0SubnetMask VariableValue "255.255.255.0"
+New-CMDeviceVariable -DeviceName BIZ-APDF103 -VariableName OSDAdapterCount -VariableValue "1"
